a6b4e43514d4e52dac94878241e67d91635cf5ff
Jasiel <Jasiel>
2025-05-29 15:22:22 -0600

    29 Mayo Control Versiones

--- src/main/java/solis/solis_hipotenusa.java
+++ src/main/java/solis/solis_hipotenusa.java
@@ -12,8 +12,8 @@ import jakarta.jws.WebParam;
  *
  * @author Jasiel
  */
-@WebService(serviceName = "solis_metodo")
-public class solis_web_service {
+@WebService(serviceName = "solis_hipotenusa")
+public class solis_hipotenusa {
 
     /**
      * This is a sample web service operation
@@ -22,11 +22,10 @@ public class solis_web_service {
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
     }
-    
-    @WebMethod(operationName = "calcular_velocidad")
-    public double calcular_velocidad(@WebParam(name = "distancia") double distancia, @WebParam(name = "tiempo")double tiempo) {
-    double velocidad=distancia/tiempo;
-    return (double)(velocidad);
+
+    @WebMethod(operationName = "calcular_hipotenusa")
+    public double calcular_hipotenusa(@WebParam(name = "cateto1") double cateto1, @WebParam(name = "cateto2") double cateto2) {
+    double hipotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+    return hipotenusa;
     }
-    
 }
